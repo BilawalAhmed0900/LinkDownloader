b01f75d6e806b9bb42249986cdbc3bf2b2435694
BilawalAhmed0900 <blwal7057@gmail.com>
2020-06-20 22:11:25 +0500

    First working version done

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const electron = require("electron");
 const {app, BrowserWindow, Menu, dialog, ipcMain} = electron;
 const ARGS = require("minimist")(process.argv.slice(2));
 const path = require("path");
-const http = require("http");
-const https = require("https");
+const http = require("follow-redirects").http;
+const https = require("follow-redirects").https;
 const url = require("url");
 const WebSocket = require("ws");
 
@@ -18,77 +18,6 @@ const WINDOW_HTML_FILE = 'mainWindow.html';
 const DEFAULT_HOST = "localhost";
 const DEFAULT_PORT = 49152;
 const DEFAULT_BACKLOG = 16;
-
-/*
-  Takes in
-    UrlString as String
-    CookiesJSON as JSON
-
-  Returns
-    Promise that may resolve to JSON containing response of HEAD request
-*/
-function getHead(UrlString, CookiesJSON)
-{
-  const module = UrlString.startsWith("https") ? https : http;
-  return new Promise((resolve, reject) =>
-  {
-    const req = module.request(UrlString, {method: "HEAD", headers: {"Cookie": CookiesJSON}}, 
-      res =>
-      {
-        resolve(res.headers);
-      });
-
-    req.on("error", error =>
-    {
-      reject(error);
-    });
-
-    req.end();
-  });
-}
-
-/*
-  Takes in
-    UrlString as string
-    HEADRequestJSON as JSON, returned from getHead
-
-  returns
-    String that contains filename to use for downloading
-*/
-function getFileName(UrlString, HEADRequestJSON)
-{
-  if ("Content-Disposition" in HEADRequestJSON)
-  {
-    const fileNameRegex = /filename=\"(.*?)\"/
-    const result = fileNameRegex.exec(HEADRequestJSON["Content-Disposition"]);
-    if (result !== null)
-    {
-      return result[1];
-    }
-  }
-
-  return UrlString.substr(UrlString.lastIndexOf("/") + 1);
-}
-
-/*
-  Takes in
-    HEADRequestJSON as JSON, returned from getHead
-
-  returns
-    bool if the url supports resuming from which HEAD has taken
-*/
-function isResumable(HEADRequestJSON)
-{
-  if ("accept-ranges" in HEADRequestJSON)
-  {
-    if (HEADRequestJSON["accept-ranges"].indexOf("bytes") !== -1)
-    {
-      return true;
-    }
-  }
-
-  return false;
-}
 
 app.on("window-all-closed", (e) =>
 {
@@ -110,91 +39,59 @@ app.on("ready", () =>
     console.log("Connection establised...");
     ws.on("message", async function incoming(data)
     {
-      const dataJSON = JSON.parse(data);
-      dataJSON["cookies"] = JSON.parse(dataJSON["cookies"]);
-
-      let headRequest;
       try
       {
-        headRequest = await getHead(dataJSON["finalUrl"], dataJSON["cookies"]);
-        dataJSON["filename"] = getFileName(dataJSON["finalUrl"], headRequest);
-      }
-      catch(error)
-      {
-        dialog.showMessageBoxSync(null, 
+        const dataJSON = JSON.parse(data);
+        const mainWindow = new BrowserWindow(
           {
-            type: "error",
-            title: "Error",
-            message: String(error)
+            width: WINDOW_WIDTH,
+            height: WINDOW_HEIGHT,
+            resizable: false,
+            show: false,
+            webPreferences: 
+            {
+              nodeIntegration: true,
+              enableRemoteModule: true
+            }
           });
-        return;
-      }
-
-      // console.log(dataJSON);
-      // console.log(headRequest);
-
-      const saveFileName = dialog.showSaveDialogSync(null, 
-        {
-          defaultPath: path.join(app.getPath("downloads"), dataJSON["filename"]),
-          properties: ["createDirectory", "dontAddToRecent"]
-        });
-
-      if (saveFileName === undefined)
-      {
-        return;
-      }
-      
-      const mainWindow = new BrowserWindow(
-        {
-          width: WINDOW_WIDTH,
-          height: WINDOW_HEIGHT,
-          resizable: false,
-          show: false,
-          webPreferences: 
-          {
-            nodeIntegration: true
-          }
-        });
-      
-      mainWindow.loadURL(url.format(
-      {
-        pathname: path.join(__dirname, WINDOW_HTML_FILE),
-        protocol: "file:",
-        slashes: true
-      }));
-    
-      mainWindow.on("ready-to-show", () =>
-      {
-        mainWindow.show();
-      });
-
-      Menu.setApplicationMenu(null);
-      /*
-        After the service gets ready, send necessary data for the electron window for
-        downloading process
-      */
-      mainWindow.webContents.on("did-finish-load", () =>
-      {
-        ipcMain.on("close-mainwindow", (_event, message) =>
-        {
-          if (message === true)
-          {
-            mainWindow.close();
-          }
-        });
         
-        mainWindow.webContents.send("downloading-data", JSON.stringify(
+        mainWindow.loadURL(url.format(
         {
-          filename: dataJSON["filename"],
-          url: dataJSON["finalUrl"],
-          length: 
-            headRequest["content-length"] !== undefined 
-            ? parseInt(headRequest["content-length"], 10)
-            : -1,
-          cookies: JSON.stringify(dataJSON["cookies"]),
-          is_resumable: isResumable(headRequest)
+          pathname: path.join(__dirname, WINDOW_HTML_FILE),
+          protocol: "file:",
+          slashes: true
         }));
-      });
+      
+        mainWindow.on("ready-to-show", () =>
+        {
+          mainWindow.show();
+          // mainWindow.toggleDevTools();
+        });
+
+        Menu.setApplicationMenu(null);
+        /*
+          After the service gets ready, send necessary data for the electron window for
+          downloading process
+        */
+        mainWindow.webContents.on("did-finish-load", () =>
+        { 
+          mainWindow.webContents.send("downloading-data", JSON.stringify(
+          {
+            url: dataJSON["finalUrl"],
+            cookies: dataJSON["cookies"],
+            userAgent: dataJSON["userAgent"]
+          }));
+        });
+      }
+      catch (_error)
+      {
+        
+      }
     });
+  });
+
+  wss.on("close", () =>
+  {
+    console.log("Connection closed...");
   });
 });
--- mainWindow.js
+++ mainWindow.js
@@ -1,8 +1,117 @@
 "use strict";
 const electron = require("electron");
-const {app, ipcRenderer} = electron;
+const {app, ipcRenderer, remote, dialog} = electron;
+const path = require("path");
 const http = require("http");
 const https = require("https");
+const fs = require("fs");
+const crypto = require("crypto");
+
+/*
+  These are all information about the file to download from the Url send from ipcMain
+  These are global variables so, whole page knows about them
+*/
+let filenameString = null;
+let UrlString;
+let CookieString;
+let UserAgentString;
+let length;
+let lengthString;
+let mainHttpRequest = null;
+
+/*
+  This will let us know, if we have paused the downloading or it has completed, after
+  the client is destroyed
+*/
+let isPaused = false;
+
+function download()
+{
+  downloadFromURL(UrlString, CookieString, UserAgentString).then(async([tempFilePath, actualFilePath]) =>
+  {
+    if (isPaused === false)
+    {
+      const {size} = fs.statSync(tempFilePath);
+
+      document.title = `Copying to ${actualFilePath}...`;
+      document.getElementById("progressBar").value = 0;
+      const readStream = fs.createReadStream(tempFilePath);
+
+      let copied = 0;
+      readStream.on("data", chunk =>
+      {
+        copied += chunk.length;
+        document.getElementById("progressBar").value = (copied / size) * document.getElementById("progressBar").max;
+      });
+
+      const stream = readStream.pipe(fs.createWriteStream(actualFilePath));
+
+      /*
+        Wait for piping to finish
+      */
+      await new Promise(fullfill => stream.on("finish", fullfill));
+      stream.close();
+      readStream.close();
+      fs.unlinkSync(tempFilePath);
+      remote.getCurrentWindow().close();
+    }
+  }).catch((error) =>
+  {
+    remote.dialog.showMessageBoxSync(remote.getCurrentWindow(), 
+    {
+      type: "error",
+      title: "Error",
+      message: String(error)
+    });
+    remote.getCurrentWindow().close();
+  });
+}
+
+/*
+  If we click the cancel button, terminate the connection and closes the window
+*/
+document.getElementById("cancelButton").addEventListener("click", event =>
+{
+  event.preventDefault();
+
+  if (remote.dialog.showMessageBoxSync(remote.getCurrentWindow(), 
+  {
+    type: "question",
+    buttons: ["Yes", "No"],
+    title: "Question",
+    message: "Are you sure you want to cancel?"
+  }) === 0)
+  {
+    if (mainHttpRequest !== null)
+    {
+      isPaused = true;
+      mainHttpRequest.destroy();
+      remote.getCurrentWindow().close();
+    }
+  }
+});
+
+document.getElementById("pauseButton").addEventListener("click", event =>
+{
+  event.preventDefault();
+
+  if (document.getElementById("pauseButton").innerHTML === "Pause")
+  {
+    document.getElementById("pauseButton").disabled = true;
+    isPaused = true;
+    mainHttpRequest.destroy();
+    document.getElementById("pauseButton").innerHTML = "Resume";
+    document.getElementById("pauseButton").disabled = false;
+  }
+  else if (document.getElementById("pauseButton").innerHTML === "Resume")
+  {
+    document.getElementById("pauseButton").disabled = true;
+    isPaused = false;
+    document.getElementById("pauseButton").innerHTML = "Pause";
+    download();
+    document.getElementById("pauseButton").disabled = false;
+  }
+});
 
 function normalizeSize(sizeInBytes, numberOfDecimalDigits = 4)
 {
@@ -27,39 +136,162 @@ function normalizeSize(sizeInBytes, numberOfDecimalDigits = 4)
   return `${result.toFixed(numberOfDecimalDigits)} ${suffixArray[suffixArrayIndex]}`;
 }
 
-let filenameString;
-let UrlString;
-let CookiesJSON;
-let isResumable;
-let length;
-let downloaded;
-ipcRenderer.on("downloading-data", (_event, message) =>
+function downloadFromURL(UrlString, CookieString, UserAgentString)
+{
+  return new Promise((resolve, reject) =>
+  {
+    /*
+      Make a unique temporary file name for each link using sha256
+    */
+    const hash = crypto.createHash("sha256");
+    const tempFileName = path.join(remote.app.getPath("downloads"), hash.update(UrlString).digest("hex"));
+    //console.log(tempFileName);
+    const startByte = fs.existsSync(tempFileName) ? fs.statSync(tempFileName).size : 0;
+    //console.log(startByte);
+
+    const module = (UrlString.startsWith("https")) ? https : http;
+    mainHttpRequest = module.get(UrlString, 
+      {
+        headers: 
+          {
+            "Cookie": CookieString,
+            "User-Agent": UserAgentString,
+            "Range": `bytes=${startByte}-`
+          }
+      }, res =>
+    {
+      const {statusCode, headers} = res;
+      //console.log(statusCode);
+      //console.log(headers);
+      /*
+        Get the file size and update GUI accordingly
+      */
+      if ("content-length" in headers)
+      {
+        length = parseInt(headers["content-length"]);
+        lengthString = normalizeSize(length);
+      }
+      else
+      {
+        length = -1;
+        lengthString = "(Unknown)";
+      }
+      document.getElementById("setSizeTo").innerHTML = lengthString;
+
+      /*
+        At first time, it is null, we will extract from content-disposition and ask user,
+        next time, we will just use the previous one for occasions like pausing
+      */
+      if (filenameString === null)
+      {
+        if ("content-disposition" in headers)
+        {
+          const filenameRegex = /filename=\"(.*?)\"/;
+          const resultant = filenameRegex.exec(headers["content-disposition"]);
+          if (resultant !== null)
+          {
+            filenameString = resultant[1];
+          }
+          else
+          {
+            filenameString = UrlString.substr(UrlString.lastIndexOf("/") + 1);
+          }
+        }
+        else
+        {
+          filenameString = UrlString.substr(UrlString.lastIndexOf("/") + 1);
+        }
+  
+        const toDownloadTo = remote.dialog.showSaveDialogSync(remote.getCurrentWindow(),
+        {
+          defaultPath: path.join(remote.app.getPath("downloads"), filenameString),
+          properties: ["createDirectory", "dontAddToRecent"]
+        });
+  
+        if (toDownloadTo === undefined)
+        {
+          res.resume();
+          isPaused = true;
+          mainHttpRequest.destroy();
+          remote.getCurrentWindow().close();
+        }
+        filenameString = toDownloadTo;
+      }
+      document.title = `Downloading ${filenameString}...`;
+
+      let file = null;
+      /*
+        If the result is 206, we append to previous created file, else we create new one
+      */
+      if (statusCode === 206)
+      {
+        document.getElementById("setResumableTo").innerHTML = "Yes";
+        file = fs.createWriteStream(tempFileName, 
+          {
+            flags: "a"
+          });
+      } 
+      else if (statusCode === 200)
+      {
+        document.getElementById("setResumableTo").innerHTML = "No";
+        file = fs.createWriteStream(tempFileName, 
+          {
+            flags: "w"
+          });
+      }
+      else
+      {
+        res.resume();
+        mainHttpRequest.destroy();
+        reject(`HTTP status code: ${statusCode}`);
+      }
+
+      let downloaded = startByte;
+      let startTime = process.hrtime();
+      res.on("data", chunk =>
+      {
+        const elapsedTime = process.hrtime(startTime);
+        const previousDownloaded = downloaded;
+        downloaded += chunk.length;
+
+        document.getElementById("setSpeedTo").innerHTML = 
+          `${normalizeSize((downloaded - previousDownloaded) / (elapsedTime[0] + (elapsedTime[1] / 1e9)))}/s`;
+
+        document.getElementById("setDownloadedTo").innerHTML = normalizeSize(downloaded);
+        if (length !== -1)
+        {
+          document.getElementById("progressBar").value = (downloaded / length) * 
+            document.getElementById("progressBar").max;
+        }
+        file.write(chunk);
+
+        startTime = process.hrtime();
+      });
+      
+      res.on("end", () =>
+      {
+        file.close();
+        resolve([tempFileName, filenameString]);
+      });
+    })
+    .on("error", (error) =>
+      {
+        reject(error);
+      });
+  });
+}
+
+ipcRenderer.on("downloading-data", async(_event, message) =>
 {
   const messageJSON = JSON.parse(message);
-  if (!("filename" in messageJSON) || !("url" in messageJSON) ||
-    !("cookies" in messageJSON) || !("is_resumable" in messageJSON) ||
-    !("length" in messageJSON))
-  {
-    ipcRenderer.send("close-mainwindow", true);
-	return;
-  }
-  messageJSON["cookies"] = JSON.parse(messageJSON["cookies"]);
+  //console.log(messageJSON);
 
-  filenameString = messageJSON["filename"];
   UrlString = messageJSON["url"];
+  CookieString = messageJSON["cookies"];
+  UserAgentString = messageJSON["userAgent"];
 
   document.getElementById("setUrlTo").innerHTML = UrlString;
-
-  CookiesJSON = messageJSON["cookies"];
-  isResumable = messageJSON["is_resumable"];
-  length = messageJSON["length"];
-
-  document.getElementById("setSizeTo").innerHTML = (length === -1) ? "(Unknown)" : normalizeSize(length);
-  document.getElementById("setResumableTo").innerHTML = (isResumable) ? "Yes" : "No";
-
-  downloaded = 0;
-  document.getElementById("setDownloadedTo").innerHTML = normalizeSize(downloaded);
-
-  document.title = `Downloading ${filenameString}...`;
+  document.getElementById("setDownloadedTo").innerHTML = normalizeSize(0);
+  download();
 });
 
